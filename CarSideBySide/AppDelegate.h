19369159203ac33e989f59fa2d5e04e77c1e1bea
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-05-28 21:52:59 -0500

    Initial Commit

---
+++ CarSideBySide/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CarSideBySide
+//
+//  Created by Alejandro Juarez Robles on 5/28/13.
+//  Copyright (c) 2013 Alejandro Juarez Robles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CarSideBySideTests/CarSideBySideTests.h
@@ -0,0 +1,13 @@
+//
+//  CarSideBySideTests.h
+//  CarSideBySideTests
+//
+//  Created by Alejandro Juarez Robles on 5/28/13.
+//  Copyright (c) 2013 Alejandro Juarez Robles. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CarSideBySideTests : SenTestCase
+
+@end
