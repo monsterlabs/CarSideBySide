632e7d5930a5a7f97e70bb866a0113e9655dc15f
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-05-29 13:49:10 -0500

    A dummy implementation for OfferListViewController was added

---
+++ CarSideBySide/OfferListViewController.h
@@ -0,0 +1,13 @@
+//
+//  OfferListViewController.h
+//  CarSideBySide
+//
+//  Created by Alejandro Juarez on 5/29/13.
+//  Copyright (c) 2013 Alejandro Juarez Robles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OfferListViewController : UITableViewController
+
+@end
