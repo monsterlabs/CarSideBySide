3f8efb09bb048c2747cbd7e5130798bebc61282f
Alejandro Juarez Robles <juarlex@gmail.com>
2013-05-28 21:52:59 -0500

    Initial Commit

---
+++ CarSideBySideTests/CarSideBySideTests.m
@@ -0,0 +1,32 @@
+//
+//  CarSideBySideTests.m
+//  CarSideBySideTests
+//
+//  Created by Alejandro Juarez Robles on 5/28/13.
+//  Copyright (c) 2013 Alejandro Juarez Robles. All rights reserved.
+//
+
+#import "CarSideBySideTests.h"
+
+@implementation CarSideBySideTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CarSideBySideTests");
+}
+
+@end
