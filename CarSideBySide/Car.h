ca752a53a6df77bce264f961a4b97e74923e7960
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-05-28 23:48:03 -0500

    Initial Data Model and Model classes were added

---
+++ CarSideBySide/Car.h
@@ -0,0 +1,28 @@
+//
+//  Car.h
+//  CarSideBySide
+//
+//  Created by Alejandro Juarez Robles on 5/28/13.
+//  Copyright (c) 2013 Alejandro Juarez Robles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Line;
+
+@interface Car : NSManagedObject
+
+@property (nonatomic, retain) NSString * modelName;
+@property (nonatomic, retain) NSNumber * year;
+@property (nonatomic, retain) NSString * image;
+@property (nonatomic, retain) NSString * largeImage;
+@property (nonatomic, retain) NSNumber * valid;
+@property (nonatomic, retain) NSDate * validUntil;
+@property (nonatomic, retain) NSNumber * priceList;
+@property (nonatomic, retain) NSNumber * retailPrice;
+@property (nonatomic, retain) NSNumber * orderKey;
+@property (nonatomic, retain) NSNumber * hp;
+@property (nonatomic, retain) Line *line;
+
+@end
