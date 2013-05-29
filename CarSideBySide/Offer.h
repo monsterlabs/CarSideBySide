1a4efcd742ea33cb65e7418b036c98f885a23826
Alejandro Juarez Robles <juarlex@gmail.com>
2013-05-29 10:24:15 -0500

    Offer model has been added

---
+++ CarSideBySide/Offer.h
@@ -0,0 +1,21 @@
+//
+//  Offer.h
+//  CarSideBySide
+//
+//  Created by Alejandro Juarez on 5/29/13.
+//  Copyright (c) 2013 Alejandro Juarez Robles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Offer : NSManagedObject
+
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * body;
+@property (nonatomic, retain) NSString * url;
+@property (nonatomic, retain) NSString * image;
+@property (nonatomic, retain) NSDate * validUntil;
+
+@end
---
+++ CarSideBySide/Offer.m
@@ -0,0 +1,20 @@
+//
+//  Offer.m
+//  CarSideBySide
+//
+//  Created by Alejandro Juarez on 5/29/13.
+//  Copyright (c) 2013 Alejandro Juarez Robles. All rights reserved.
+//
+
+#import "Offer.h"
+
+
+@implementation Offer
+
+@dynamic title;
+@dynamic body;
+@dynamic url;
+@dynamic image;
+@dynamic validUntil;
+
+@end
