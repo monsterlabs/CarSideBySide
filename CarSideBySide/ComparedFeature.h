df2e7bf2366388dcba2d0d491f3496f17755cd95
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-02 10:48:12 -0500

    The ComparedFeature model was added and the Comparative, ComparedCar, Feature and Specification models were refactored. Also, the CoreDataSeed class was improved

---
+++ CarSideBySide/ComparedFeature.h
@@ -0,0 +1,20 @@
+//
+//  ComparedFeature.h
+//  CarSideBySide
+//
+//  Created by Alejandro Juarez Robles on 7/2/13.
+//  Copyright (c) 2013 Alejandro Juarez Robles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Comparative, Feature;
+
+@interface ComparedFeature : NSManagedObject
+
+@property (nonatomic, retain) NSString * descr;
+@property (nonatomic, retain) Feature *feature;
+@property (nonatomic, retain) Comparative *comparative;
+
+@end
