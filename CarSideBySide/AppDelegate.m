942557a4c3c7ba8909b8324b862e5d68c626a0ab
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-21 00:13:50 -0500

    First implementation of CarCatalogAPIClient and CarSideBySideIncrementalStore based on the AFIncrementalStore and AFNetworking cocoapods

--- CarSideBySide/AppDelegate.m
+++ CarSideBySide/AppDelegate.m
@@ -10,8 +10,9 @@
 #import <YISplashScreen.h>
 #import <YISplashScreen+Migration.h>
 #import <YISplashScreenAnimation.h>
+#import "CarSideBySideIncrementalStore.h"
 #import "CoreDataSeed.h"
-#define SHOWS_MIGRATION_ALERT 1
+#define SHOWS_MIGRATION_ALERT 0
 #define USES_PRESET_ANIMATION 1
 
 
@@ -102,7 +103,9 @@
     
     NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
     if (coordinator != nil) {
-        _managedObjectContext = [[NSManagedObjectContext alloc] init];
+//        _managedObjectContext = [[NSManagedObjectContext alloc] init];
+        _managedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+
         [_managedObjectContext setPersistentStoreCoordinator:coordinator];
     }
     return _managedObjectContext;
@@ -127,38 +130,26 @@
     if (_persistentStoreCoordinator != nil) {
         return _persistentStoreCoordinator;
     }
+
+    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+
+    AFIncrementalStore *incrementalStore = (AFIncrementalStore *)[_persistentStoreCoordinator addPersistentStoreWithType:[CarSideBySideIncrementalStore type] configuration:nil URL:nil options:nil error:nil];
     
     NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"CarSideBySide.sqlite"];
     
+    NSDictionary *options = @{
+                              NSInferMappingModelAutomaticallyOption : @(YES),
+                              NSMigratePersistentStoresAutomaticallyOption: @(YES)
+                              };
+    
     NSError *error = nil;
-    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
-        /*
-         Replace this implementation with code to handle the error appropriately.
-         
-         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-         
-         Typical reasons for an error here include:
-         * The persistent store is not accessible;
-         * The schema for the persistent store is incompatible with current managed object model.
-         Check the error message to determine what the actual problem was.
-         
-         
-         If the persistent store is not accessible, there is typically something wrong with the file path. Often, a file URL is pointing into the application's resources directory instead of a writeable directory.
-         
-         If you encounter schema incompatibility errors during development, you can reduce their frequency by:
-         * Simply deleting the existing store:
-         [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil]
-         
-         * Performing automatic lightweight migration by passing the following dictionary as the options parameter:
-         @{NSMigratePersistentStoresAutomaticallyOption:@YES, NSInferMappingModelAutomaticallyOption:@YES}
-         
-         Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
-         
-         */
+    if (![incrementalStore.backingPersistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error]) {
         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
         abort();
     }
+    
+    NSLog(@"SQLite URL: %@", storeURL);
+    
     
     return _persistentStoreCoordinator;
 }
---
+++ CarSideBySide/CarSideBySideIncrementalStore.h
@@ -0,0 +1,14 @@
+//
+//  OfferIncrementalStore.h
+//  CarSideBySide
+//
+//  Created by Alejandro Juarez Robles on 7/20/13.
+//  Copyright (c) 2013 Alejandro Juarez Robles. All rights reserved.
+//
+
+#import "AFIncrementalStore.h"
+
+@interface CarSideBySideIncrementalStore : AFIncrementalStore
+
+
+@end
