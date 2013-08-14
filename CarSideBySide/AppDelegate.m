bead99ff90e557cf6cfde9b08b1342e4450d6912
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-08-14 02:08:23 -0500

    Removing old CoreData code

--- CarSideBySide/AppDelegate.m
+++ CarSideBySide/AppDelegate.m
@@ -10,42 +10,30 @@
 #import <YISplashScreen.h>
 #import <YISplashScreen+Migration.h>
 #import <YISplashScreenAnimation.h>
-#import "CoreDataSeed.h"
 #import "CoreDataStack.h"
-#define SHOWS_MIGRATION_ALERT 1
-#define USES_PRESET_ANIMATION 0
 
 
 @implementation AppDelegate
 
 @synthesize managedObjectContext = _managedObjectContext;
-@synthesize managedObjectModel = _managedObjectModel;
-@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
 @synthesize coreDataStack = _coreDataStack;
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{    
+{
     [YISplashScreen show];
+    
     _coreDataStack = [CoreDataStack coreDataStackWithModelName:@"CarSideBySide"];
     _coreDataStack.coreDataStoreType = CDSStoreTypeSQL;
     _managedObjectContext = _coreDataStack.managedObjectContext;
-#if SHOWS_MIGRATION_ALERT
-    void (^migrationBlock)(void) = ^{
-        CoreDataSeed *seed = [[CoreDataSeed alloc] init];
-        [seed loadInitialData];
-    };
-#else
-    void (^migrationBlock)(void) = nil;
-#endif
     
-    [YISplashScreen waitForMigration:migrationBlock completion:^{
+    [YISplashScreen waitForMigration:nil completion:^{
         [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationFade];
-        
         
         UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
         if (orientation == UIInterfaceOrientationPortrait || orientation == UIInterfaceOrientationPortraitUpsideDown)
             [YISplashScreen hideWithAnimation:[YISplashScreenAnimation cubeAnimation]];
         else
-            [YISplashScreen hideWithAnimation:[YISplashScreenAnimation fadeOutAnimation]];
+            [YISplashScreen hideWithAnimation:[YISplashScreenAnimation pageCurlAnimation]];
         
     }];
     
@@ -78,102 +66,5 @@
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
-
-- (void)saveContext
-{
-    NSError *error = nil;
-        [self.coreDataStack saveOrFail:^(NSError* error) {
-             NSLog(@"There was an error %@", error);
-             NSLog(@"Failed to save to data store: %@", [error localizedDescription]);
-         }];
-//        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
-//            // Replace this implementation with code to handle the error appropriately.
-//            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-//            abort();
-//        }
-}
-//
-//#pragma mark - Core Data stack
-//
-//// Returns the managed object context for the application.
-//// If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
-//- (NSManagedObjectContext *)managedObjectContext
-//{
-//    if (_managedObjectContext != nil) {
-//        return _managedObjectContext;
-//    }
-//    
-//    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
-//    if (coordinator != nil) {
-//        _managedObjectContext = [[NSManagedObjectContext alloc] init];
-//        [_managedObjectContext setPersistentStoreCoordinator:coordinator];
-//    }
-//    return _managedObjectContext;
-//}
-//
-//// Returns the managed object model for the application.
-//// If the model doesn't already exist, it is created from the application's model.
-//- (NSManagedObjectModel *)managedObjectModel
-//{
-//    if (_managedObjectModel != nil) {
-//        return _managedObjectModel;
-//    }
-//    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"CarSideBySide" withExtension:@"momd"];
-//    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
-//    return _managedObjectModel;
-//}
-//
-//// Returns the persistent store coordinator for the application.
-//// If the coordinator doesn't already exist, it is created and the application's store added to it.
-//- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
-//{
-//    if (_persistentStoreCoordinator != nil) {
-//        return _persistentStoreCoordinator;
-//    }
-//    
-//    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"CarSideBySide.sqlite"];
-//    
-//    NSError *error = nil;
-//    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-//    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
-//        /*
-//         Replace this implementation with code to handle the error appropriately.
-//         
-//         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//         
-//         Typical reasons for an error here include:
-//         * The persistent store is not accessible;
-//         * The schema for the persistent store is incompatible with current managed object model.
-//         Check the error message to determine what the actual problem was.
-//         
-//         
-//         If the persistent store is not accessible, there is typically something wrong with the file path. Often, a file URL is pointing into the application's resources directory instead of a writeable directory.
-//         
-//         If you encounter schema incompatibility errors during development, you can reduce their frequency by:
-//         * Simply deleting the existing store:
-//         [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil]
-//         
-//         * Performing automatic lightweight migration by passing the following dictionary as the options parameter:
-//         @{NSMigratePersistentStoresAutomaticallyOption:@YES, NSInferMappingModelAutomaticallyOption:@YES}
-//         
-//         Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
-//         
-//         */
-//        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-//        abort();
-//    }
-//    
-//    return _persistentStoreCoordinator;
-//}
-//
-//#pragma mark - Application's Documents directory
-//
-//// Returns the URL to the application's Documents directory.
-//- (NSURL *)applicationDocumentsDirectory
-//{
-//    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
-//}
-
 
 @end
